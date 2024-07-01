f905bfabe46855c0f84e91b6f34395ba4ea34df1
sounak <sounak.peter@gmail.com>
2024-07-02 02:25:49 +0530

    Creating joke controller. Closes #3

---
+++ src/main/java/sounak/springframework/spring5_jokes_app/controllers/JokeController.java
@@ -0,0 +1,26 @@
+package sounak.springframework.spring5_jokes_app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import sounak.springframework.spring5_jokes_app.services.JokeService;
+
+/**
+ * Created by sounak on 01-07-2024.
+ */
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String getJoke(Model model){
+        model.addAttribute("joke", jokeService.getJoke());
+        return "index";
+    }
+
+}
